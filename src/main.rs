7fb960f846e5fdc51a5ecaa5c3aac080b19c0795
Ythern <eternalisime@gmail.com>
2023-05-10 13:28:34 +0200

    base implementation of reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use reqwest;
+
+#[tokio::main]
+async fn main() {
+    let directory = "D:/Musique";
+    let blacklist = vec![
+        String::from("Various Artists"),
+        String::from("Music Bee")
+
+    ];
+    let key = "pySEUWvBbSVmDRIsxvno";
+    let secret = "KQlCvdHDnEMYpDJtZMqHRRktSMZqxMQP";
+    let len = directory.len() + 1;
+    let paths = fs::read_dir(directory).unwrap();
+    let client = reqwest::Client::builder()
+        .user_agent("ythern")
+        .build();
+
+    for path in paths {
+        let path_string = path.unwrap().path().display().to_string().split_off(len);
+        let path_str = &path_string;
+        if !blacklist.contains(path_str) {
+            let url = &("https://api.discogs.com/database/search?q=".to_owned() +
+                &str::replace(path_str, " ", "%20")
+                + "&type=artist&key=" + key
+                + "&secret=" + secret);
+            let body = client.get(url)
+                .send()
+                .await;
+            println!("body = {:?}", body);
+            println!("{}",url)
+        }
+    }
 }
+
+
+
+
