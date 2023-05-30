127915ae9a6992f93409d2e0ea2676fbf3c8b180
Ythern <eternalisime@gmail.com>
2023-05-30 15:51:09 +0200

    feat: changed API from Discog to Musicbrainz to fix release issue

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 use reqwest;
 use std::thread;
 use std::time::Duration;
 
 #[tokio::main]
 async fn main() {
+    let mut data_file = File::create("download_list.txt").expect("creation failed");
     let mut i = 0;
-    let one_sec = Duration::new(1,0);
-    let directory = "C:/Users/Ythern/Documents/Musique";
+    let one_sec = Duration::new(2,0);
+    let directory = "D:/Musique";
     let blacklist = vec![
         String::from("Various Artists"),
-        String::from("Music Bee")
+        String::from("Music Bee"),
+        String::from("Gogojuice")
     ];
-    let key = "pySEUWvBbSVmDRIsxvno";
-    let secret = "KQlCvdHDnEMYpDJtZMqHRRktSMZqxMQP";
     let len = directory.len() + 1;
     let paths = fs::read_dir(directory).unwrap();
     let client = reqwest::Client::builder()
@@ -29,13 +31,13 @@ async fn main() {
         let mut owned_album = Vec::new();
         for path_artist in paths_artist {
             let path_artist_string = path_artist.unwrap().path().display().to_string().split_off(artist_len);
-            owned_album.push(remove_whitespace(&path_artist_string));
+            owned_album.push(remove_whitespace(&(&path_artist_string)));
         }
         if !blacklist.contains(path_str) {
-            let url = &("https://api.discogs.com/database/search?q=".to_owned() +
+            let url = &("https://musicbrainz.org/ws/2/artist/?query=".to_owned() +
                 &str::replace(path_str, " ", "%20")
-                + "&type=artist&key=" + key
-                + "&secret=" + secret);
+                + "&fmt=json");
+            println!("{}", url);
             let body = client.get(url)
                 .send()
                 .await
@@ -43,9 +45,11 @@ async fn main() {
                 .json::<serde_json::Value>()
                 .await;
             thread::sleep(one_sec);
-            let id = &body.unwrap()["results"][0]["id"];
-            let url_artist = &("https://api.discogs.com/artists/".to_owned() + &id.to_string()
-                + "/releases");
+            let id = body.unwrap()["artists"][0]["id"].as_str().unwrap().to_owned();
+            let url_artist = &("https://musicbrainz.org/ws/2/release-group?query=%22%22%20AND%20arid:".to_owned() +
+                &id
+                + "%20NOT%20primarytype:single%20NOT%20secondarytype:*&fmt=json");
+            println!("{}", url_artist);
             let release_list = client.get(url_artist)
                 .send()
                 .await
@@ -53,15 +57,16 @@ async fn main() {
                 .json::<serde_json::Value>()
                 .await;
             thread::sleep(one_sec);
-            for releases in release_list.unwrap()["releases"].as_array(){
+            for releases in release_list.unwrap()["release-groups"].as_array(){
                 let len = releases.len();
                 while i < len {
-                    let album = releases[i]["title"].as_str().unwrap().to_string();
+                    let album = &releases[i]["title"].as_str().unwrap().to_string();
                     if !owned_album.contains(&remove_whitespace(&album)) {
-                        println!("{} - {}", path_str, album)
+                        data_file.write((path_str.to_owned() + " - " + album + "\n").as_bytes()).expect("write failed");
                     }
                     i += 1;
                 }
+                i = 0;
             }
         }
     }
